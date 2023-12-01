545c3f0cbd0c60d464435892b706f2bb5c693499
Christopher Trauco <chris@trau.co>
2023-12-01 00:46:08 -0500

    correction to env and object attribute mapping master ss

--- etl.sh
+++ etl.sh
@@ -4,6 +4,6 @@ python3 response.py | tr -d '"' | tr -d '{' | tr -d '}' | awk '{ if($0 !~ /^[[:s
 
 python3 entities.py | tr -d '"' | tr -d '{' | tr -d '}' | awk '{ if($0 !~ /^[[:space:]]*$/) print $0 }' | sed '$!s/$/,/' | sed 's/^ *//g' > entities.csv
 
-python3 list_intents.py | tr -d '"' | tr -d '{' | tr -d '}' | awk '{ if($0 !~ /^[[:space:]]*$/) print $0 }' | sed '$!s/$/,/' | sed 's/^ *//g' > list_intents.csv
+python3 list_intents.py | tr -d '"' | tr -d '{' | tr -d '}' | awk '{ if($0 !~ /^[[:space:]]*$/) print $0 }'  | sed 's/^ *//g' > intents.csv
 
 # python3 list_intents.py | tr -d '"' | tr -d '{' | tr -d '}' | awk '{ if($0 !~ /^[[:space:]]*$/) print $0 }'  | sed 's/^ *//g' > intents.csv
